3a1859f3731bd4dc77e5519ffa6ded14366595a9
stadzhibov <STajibov@alfabank.ru>
2024-04-14 16:51:12 +0000

    запушил код

--- main.cpp
+++ main.cpp
@@ -1,48 +1,41 @@
 #include "mbed.h"
+#include "encoder.h"
+#include "DcServo.h"
 
 #include <ros.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Int32.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <geometry_msgs/Vector3.h>  
-#include "Encoder.h"
-#include "MotorDriver.h"
-#include "MotorController.h"
-#include "PID.h"
 
+#include "config.h"
 #define PUB_RATE 0.1
 
 float kp = 5;
 float ki = 0.5;
 float kd = 0;
-float td = 0.3;
 float setpoint = 0;
 
-Timer workingTimer;
-
-PID pid_motor1(kp, ki, kd, td);
-Encoder enc_motor1(D6, D7, 100, 16, Encoder::X4_ENCODING);
-MotorDriver driver_motor1(D3, D4, D5);
-MotorController contoller_motor1(&driver_motor1, &enc_motor1, &workingTimer, &pid_motor1);
-
 Ticker motor_info_ticker;
+
+Encoder encoder{ENCA_PIN, ENCB_PIN, REDUCTER_RATIO, ENCODER_IMPULSES, true};
+
+DcServo servo{IN1_PIN, IN2_PIN, PWM_PIN, &encoder};
+
 ros::NodeHandle nh;
 geometry_msgs::Vector3 monitoring_msg;
 
-DigitalOut led(LED1);
-
-// void pwmSub(const std_msgs::Float32 &pwm_msg);
+void pwmSub(const std_msgs::Float32 &pwm_msg);
 void setAngle(const std_msgs::Float32 &angle_msg);
-void runMotor(const std_msgs::Empty &run_msg);
 void pidTuningsCb(const geometry_msgs::Vector3 &pid_tunings_msg);
 void stopMotor(const std_msgs::Empty &stop_msg);
 void getMotorInfo(void);
 void initRosTopics(void);
 
-// ros::Subscriber<std_msgs::Float32> motor_pwm_sub("motor_pwm", &pwmSub);
+ros::Subscriber<std_msgs::Float32> motor_pwm_sub("motor_pwm", &pwmSub);
 ros::Subscriber<std_msgs::Empty> stop_motor_sub("stop_motor", &stopMotor);
-ros::Subscriber<std_msgs::Empty> run_motor_sub("run_motor", &runMotor);
 ros::Subscriber<std_msgs::Float32> set_angle_sub("set_angle", &setAngle);
 ros::Subscriber<geometry_msgs::Vector3> pid_tunings_sub("pid_tunings", &pidTuningsCb);
 
@@ -56,49 +49,47 @@ int main()
     while (1)
     {
         nh.spinOnce();
-        wait_ms(1);
     }
 }
 
 void initRosTopics()
 {
     nh.advertise(monitoring_pub);
-    // nh.subscribe(motor_pwm_sub);
+    nh.subscribe(motor_pwm_sub);
     nh.subscribe(stop_motor_sub);
     nh.subscribe(set_angle_sub);
     nh.subscribe(pid_tunings_sub);
-    nh.subscribe(run_motor_sub);
 }
 
-// void pwmSub(const std_msgs::Float32 &pwm_msg) {
-
-// }
-
-void runMotor(const std_msgs::Empty &run_msg) {
-    led = !led;
-    driver_motor1.directRevolute(0.6);
+void pwmSub(const std_msgs::Float32 &pwm_msg)
+{
+    servo.revolute(pwm_msg.data);
+    monitoring_msg.y = pwm_msg.data;
 }
 
-void stopMotor(const std_msgs::Empty &stop_msg) {
-    contoller_motor1.stopMotor();
+void stopMotor(const std_msgs::Empty &stop_msg)
+{
+    setpoint = 0;
+    servo.stop();
 }
 
-void setAngle(const std_msgs::Float32 &angle_msg) {
+void setAngle(const std_msgs::Float32 &angle_msg){
     setpoint = angle_msg.data;
-    contoller_motor1.setAngle(setpoint);
+    monitoring_msg.x = setpoint;
+    servo.setAngle(setpoint);
 }
 
-void pidTuningsCb(const geometry_msgs::Vector3 &pid_tunings_msg) {
+void pidTuningsCb(const geometry_msgs::Vector3 &pid_tunings_msg)
+{
     kp = pid_tunings_msg.x;
     ki = pid_tunings_msg.y;
     kd = pid_tunings_msg.z;
-    
-    pid_motor1.setPID(kp, ki, kd, td);
+    servo.setPid(kp, ki, kd);
 }
 
-void getMotorInfo() {
+void getMotorInfo(){
     monitoring_msg.x = setpoint;
-    monitoring_msg.y = enc_motor1.getPulses();
-    monitoring_msg.z = enc_motor1.getCurrentAngle();
+    monitoring_msg.y = servo.getAngle();
+    monitoring_msg.z = servo.getPWM();
     monitoring_pub.publish(&monitoring_msg);
 }
